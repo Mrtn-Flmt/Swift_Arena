4d6eaea1bfff03247585d96a2be1084c2556fd7e
Mrtn-Flmt <mrtn.flmt@gmail.com>
2020-04-29 23:49:16 +0200

    Initial Commit

---
+++ Swift_Arena/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift_Arena
+//
+//  Created by Martin Flament on 29/04/2020.
+//  Copyright © 2020 Martin Flament. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
